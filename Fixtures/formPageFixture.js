394ef6080a86e785d89a9e868ada413d9e3aec6a
Onkar Tadakalkar <onkart007@gmail.com>
2026-08-25 15:29:38 +0530

    Clean Main Branch with Login page code only

--- Fixtures/formPageFixture.js
+++
@@ -1,9 +0,0 @@
-import {test as base, expect} from "@playwright/test";
-import { Formpage } from "../pages/formpage.js"
-
-export const test = base.extend({
-    formpage : async({page}, use)=>{
-        await use(new Formpage(page));
-    }
-})
-export { expect };
