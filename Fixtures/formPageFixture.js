587277cc93ed03b6b71684fe7fddd71930068821
Onkar Tadakalkar <onkart007@gmail.com>
2026-08-25 15:53:15 +0530

    Test Run on Form_test branch

--- Fixtures/formPageFixture.js
+++ Fixtures/formPageFixture.js
@@ -1,6 +1,6 @@
 import {test as base, expect} from "@playwright/test";
 import { Formpage } from "../pages/formpage.js"
-
+//test field has been added
 export const test = base.extend({
     formpage : async({page}, use)=>{
         await use(new Formpage(page));
