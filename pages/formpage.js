eebd46b5e0e068b6e95ad086311689fef9097fd1
Onkar Tadakalkar <tadakalkaro@gmail.com>
2026-08-31 12:37:31 +0530

    Restoring pages

---
+++ pages/formpage.js
@@ -0,0 +1,22 @@
+import { BasePage } from './basepage.js';
+
+export class Formpage extends BasePage {
+    constructor(page) {
+        super(page);
+        this.nameField = page.locator('form input[name="name"]');
+        this.emailField = page.locator('form input[name="email"]');
+        this.passwordField = page.locator('form input[type="password"]');
+        this.submitbtn = page.getByRole('button', { name: 'Submit' });
+        this.successMessage = page.locator("//div[@class='alert alert-success alert-dismissible']");
+    }
+
+    async navigateToForm() {
+        await this.page.goto('https://rahulshettyacademy.com/angularpractice/');
+    }
+
+    async fillFormData(name, email, password) {
+        await this.enterText(this.nameField, name);
+        await this.enterText(this.emailField, email);
+        await this.enterText(this.passwordField, password);
+    }
+}
