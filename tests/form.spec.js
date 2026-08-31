4465bf2a0e5c31935c949c8eb84ed76d57c97307
Onkar Tadakalkar <tadakalkaro@gmail.com>
2026-08-31 12:23:26 +0530

    Restore form.spec.js, still needed for dorm validation

---
+++ tests/form.spec.js
@@ -0,0 +1,20 @@
+import { test, expect } from '../Fixtures/formPageFixture.js';
+import  Loginpage  from '../pages/loginpage.js';
+import{ getFormData } from '../helpers/datahelper.js';
+
+const formData = getFormData('json');
+
+test.describe("Form Submission scenario using JSON", () => {
+    
+    for (const data of formData) {
+        
+        test(`submit form data for ${data.Name}`, async ({ page, formpage }) => {
+           const loginpage = new Loginpage(page);
+           await loginpage.loginToApplication(process.env.LOGIN_USERNAME, process.env.LOGIN_PASSWORD);
+            await formpage.navigateToForm(); 
+            await formpage.fillFormData(data.Name, data.Email, data.Password);
+            await formpage.clickElement(formpage.submitbtn);
+            await expect(formpage.successMessage).toBeVisible();
+        });
+    }
+});
