a19b2ec3fac88e1797dcd2132997912e83ba2f62
Onkar Tadakalkar <tadakalkaro@gmail.com>
2026-09-01 13:17:06 +0530

    code refinement

--- pages/formpage.js
+++ pages/formpage.js
@@ -7,11 +7,12 @@ export class Formpage extends BasePage {
         this.emailField = page.locator('form input[name="email"]');
         this.passwordField = page.locator('form input[type="password"]');
         this.submitbtn = page.getByRole('button', { name: 'Submit' });
+        this.category =  page.getByText('Category 1')
         this.successMessage = page.locator("//div[@class='alert alert-success alert-dismissible']");
     }
 
     async navigateToForm() {
-        await this.page.goto('https://rahulshettyacademy.com/angularpractice/');
+        await this.category().click();
     }
 
     async fillFormData(name, email, password) {
