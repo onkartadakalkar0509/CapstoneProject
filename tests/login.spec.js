4e9c965554e2880daf43d8a49ddff6eeaa1560dd
Onkar Tadakalkar <tadakalkaro@gmail.com>
2026-09-01 16:46:46 +0530

    Test dry run

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -4,7 +4,7 @@ import { fileURLToPath } from 'url';
 import dotenv from 'dotenv';
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __dirname = path.dirname(__filename); 
 dotenv.config({ path: path.resolve(__dirname, 'env/.env.Onkar') });
 
 const USERNAME = process.env.LOGIN_USERNAME;
